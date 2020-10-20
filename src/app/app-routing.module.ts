692f97704f98bc777ba41d4de74dc7af750b2fea
Pooja <poojashenoy93@gmail.com>
2020-10-20 22:12:45 +1100

    added new component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router'; 
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { HomeComponent } from './home/home.component';
+import { AddEventComponent } from './add-event/add-event.component';
 
-const routes: Routes = [{
-  path: '', component: HomeComponent,
-}, {
-  path: 'test', component: TestComponent
-}];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'test',
+    component: TestComponent,
+  },
+  { path: 'add-event', component: AddEventComponent },
+];
 
 @NgModule({
-  declarations: [
-  ],
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
-
+export class AppRoutingModule {}
 
 // export const AppRoutingModule =  RouterModule.forRoot(routes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { TestComponent } from './test/test.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AddEventComponent } from './add-event/add-event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    HomeComponent
+    HomeComponent,
+    AddEventComponent
   ],
   imports: [
     BrowserModule,
