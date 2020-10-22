f2eca6ab327c578001767b9bb5d9b1915c8ecbd2
pooja <poojashenoy93@gmail.com>
2020-10-22 22:44:43 +1100

    input added

--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-event',
   templateUrl: './add-event.component.html',
-  styleUrls: ['./add-event.component.scss']
+  styleUrls: ['./add-event.component.scss'],
 })
 export class AddEventComponent implements OnInit {
+  eventForm: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.eventForm = new FormGroup({
+      eventName: new FormControl('', Validators.required),
+      date: new FormControl('', Validators.required),
+      // slotTimes: new FormControl(''),
+      // noOfAttendesPerSlot: new FormControl('', Validators.required),
+      noOfCourts: new FormControl('', Validators.required),
+      slotDuration: new FormControl('', Validators.required),
+      photo: new FormControl(''),
+      description: new FormControl(''),
+    });
   }
 
+  onSubmit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AddEventComponent } from './add-event/add-event.component';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent,
-    HomeComponent,
-    AddEventComponent
-  ],
+  declarations: [AppComponent, TestComponent, HomeComponent, AddEventComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
+    AppRoutingModule,
     HttpClientModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
